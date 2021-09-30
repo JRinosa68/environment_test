e7d5ea4f0484ed1c0d519b581dd0a28971899bcc
root <root@localhost>
2021-09-30 22:36:19 +0000

    Final Changes

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,17 +1,27 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(title: 'harry potter')
-  end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
+    it 'is valid with valid attributes' do
+      title = described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 12, published_date: Date.new(1997, 06, 26))
+      expect(title).to be_valid
+    end
+  
+    it 'is valid with valid attributes' do
+      author = described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 12, published_date: Date.new(1997, 06, 26))
+      expect(author).to be_valid
+    end
+  
+    it 'is valid with valid attributes' do
+      price = described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 12, published_date: Date.new(1997, 06, 26))
+      expect(price).to be_valid
+    end
 
-  it 'is not valid without a name' do
-    subject.title = nil
-    expect(subject).not_to be_valid
-  end
+    it 'is valid with valid attributes' do
+      published_date = described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 12, published_date: Date.new(1997, 06, 26))
+      expect(published_date).to be_valid
+    end    
+    
 end
